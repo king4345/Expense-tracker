a3f70b1c6aa560e0c6933a75bfe70c59ad648060
Ojas <ojassanap4345@gmail.com>
2026-02-28 12:53:43 +0000

    Added labels to input field for better UI

---
+++ script.js
@@ -0,0 +1,62 @@
+let expenses = [];
+
+function addExpense() {
+    let desc = document.getElementById("desc").value;
+    let amount = document.getElementById("amount").value;
+    let category = document.getElementById("category").value;
+
+    if (desc === "" || amount === "") {
+        alert("Enter all details");
+        return;
+    }
+
+    let expense = {
+        desc: desc,
+        amount: Number(amount),
+        category: category
+    };
+
+    expenses.push(expense);
+
+    displayExpenses(expenses);
+}
+
+function displayExpenses(data) {
+    let list = document.getElementById("list");
+    list.innerHTML = "";
+
+    let total = 0;
+
+    data.forEach((e, index) => {
+        total += e.amount;
+
+        let li = document.createElement("li");
+
+        li.innerHTML = `
+            ${e.desc} - ₹${e.amount} (${e.category})
+            <button onclick="deleteExpense(${index})">Delete</button>
+        `;
+
+        list.appendChild(li);
+    });
+
+    document.getElementById("total").innerText = total;
+}
+
+function deleteExpense(index) {
+    expenses.splice(index, 1);
+    displayExpenses(expenses);
+}
+
+function filterExpenses() {
+    let category = document.getElementById("filter").value;
+
+    let filtered = expenses.filter(e => e.category === category);
+
+    displayExpenses(filtered);
+}
+
+function clearAll() {
+    expenses = [];
+    displayExpenses(expenses);
+}
